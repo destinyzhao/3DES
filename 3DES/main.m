15549cb4dce8c1c5f609dd4697c98c1409640c42
destinyzhao <zhaojinxiong@nongxinmatoMacBook-Air.local>
2015-02-06 10:45:55 +0800

    modify
    modify

--- 3DES/main.m
+++ 3DES/main.m
@@ -15,10 +15,10 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-//        NSString *encryptResult = [Security3DES TripleDES:@"3DES加密解密测试" encryptOrDecrypt:kCCEncrypt];
-//        NSLog(@"加密:%@",encryptResult);
+        NSString *encryptResult = [Security3DES TripleDES:@"3DES加密解密测试" encryptOrDecrypt:kCCEncrypt];
+        NSLog(@"加密:%@",encryptResult);
         
-        NSString *decryptResult = [Security3DES TripleDES:@"n+eohUTlBAG6Lq7kUsaBWBRLCqW/e7erHRAh5ZhgDynQTXU4BsvU/x1aom8VG7YYsvmFqTpW4hN39NMGbHqZyZxlDpORJUTKkzw53HorxTmb1K+kv2ZmzQ==" encryptOrDecrypt:kCCDecrypt];
+        NSString *decryptResult = [Security3DES TripleDES:encryptResult encryptOrDecrypt:kCCDecrypt];
         NSLog(@"解密:%@",decryptResult);
         
     }
